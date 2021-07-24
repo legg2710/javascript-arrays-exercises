c97fd5e5ffa7aafada12390ac3f6222ff1547a4d
legg2710 <luisemilio2710@gmail.com>
2021-07-24 03:01:43 +0000

    javascript-arrays-exercises-tutorial 50%

--- exercises/08.3-Sum-all-items/app.js
+++ exercises/08.3-Sum-all-items/app.js
@@ -1,7 +1,10 @@
 function sumTheElements(theArray){
-    var total = 0;
-    for (i = 0; i < theArray.length; i = i + 1) {
-        return total;
+	
+	var total = 0;
+
+    for (i=0; i< theArray.length; i++){
+        total = total + theArray[i];
     }
+
+	return total;
 }
-sumTheElements();
